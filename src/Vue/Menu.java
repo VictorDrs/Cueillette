687004e3007cc1604d5bcf54ae8d73cef59cffc0
VictorDrs <victor.darmois@gmail.com>
2016-11-29 11:39:35 +0100

    Classe statistique, decoupage du code, changement total sur les vues, corrections de bugs

--- src/Vue/Menu.java
+++ src/Vue/Menu.java
@@ -1,17 +1,13 @@
 package Vue;
 
 import Cueillette.Modele;
-import Vue.Parametres;
+import Cueillette.Parametres;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
-
-import javax.swing.JFileChooser;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 
 @SuppressWarnings("serial")
 public class Menu extends JMenuBar {
